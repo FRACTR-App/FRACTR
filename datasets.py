c4dd8f2ac013add5486adece353dcf91b422c872
John Cambefort <jcambefort@middlebury.edu>
2021-03-28 16:22:20 -0400

    Streamline data download from the API to the output of geojson using gpd

--- datasets.py
+++ datasets.py
@@ -1,36 +1,43 @@
 import requests
 import json
 
-#make the request for E-911 hydrant data from VT Geoportal
-r = requests.get('https://opendata.arcgis.com/datasets/8429ff7bc1ba4abeb37d6c8ae49c8a8c_26.geojson')
-if not(r.raise_for_status()):
-    #Create the json object
-    hydrant_data = r.json()
-    #Write the object to file.
-    with open('hydrants.json','w') as jsonFile:
-        json.dump(hydrant_data, jsonFile)
-else:
-    print("An error occurred while trying to retrieve hydrant data")
+api_hydrants_path = "hydrants.json"
+api_zones_path = "zones.json"
+api_structures_path = "structures.json"
+
+# Downloads data as JSON files from the Vermont Geoportal REST API
+def request_API_data():
+    # Make the request for E-911 hydrant data from VT Geoportal
+    r = requests.get('https://opendata.arcgis.com/datasets/8429ff7bc1ba4abeb37d6c8ae49c8a8c_26.geojson')
+    if not(r.raise_for_status()):
+        # Create the json object
+        hydrant_data = r.json()
+        # Write the object to file.
+        with open(api_hydrants_path,'w') as jsonFile:
+            json.dump(hydrant_data, jsonFile)
+    else:
+        print("An error occurred while trying to retrieve hydrant data")
 
 
-#make the request for E-911 service zone data from VT Geoportal
-r = requests.get('https://opendata.arcgis.com/datasets/777ccbd85d8f4047906e37b0d16bf1e1_28.geojson')
-if not(r.raise_for_status()):
-    #Create the json object
-    zone_data = r.json()
-    #Write the object to file.
-    with open('zone_data.json','w') as jsonFile:
-        json.dump(zone_data, jsonFile)
-else:
-    print("An error occurred while trying to retrieve service zone data")
+    # make the request for E-911 service zone data from VT Geoportal
+    r = requests.get('https://opendata.arcgis.com/datasets/777ccbd85d8f4047906e37b0d16bf1e1_28.geojson')
+    if not(r.raise_for_status()):
+        # Create the json object
+        zone_data = r.json()
+        # Write the object to file.
+        with open(api_zones_path,'w') as jsonFile:
+            json.dump(zone_data, jsonFile)
+    else:
+        print("An error occurred while trying to retrieve service zone data")
 
-#make the request for E-911 structure data from VT Geoportal
-r = requests.get('https://opendata.arcgis.com/datasets/7a393abbbaa941449630361d9fd153c4_29.geojson')
-if not(r.raise_for_status()): 
-    #Create the json object
-    address_data = r.json()
-    #Write the object to file.
-    with open('address_data.json','w') as jsonFile:
-        json.dump(address_data, jsonFile)
-else:
-    print("An error occurred while trying to retrieve structure data")
+
+    # make the request for E-911 structure data from VT Geoportal
+    r = requests.get('https://opendata.arcgis.com/datasets/7a393abbbaa941449630361d9fd153c4_29.geojson')
+    if not(r.raise_for_status()): 
+        # Create the json object
+        structures_data = r.json()
+        # Write the object to file.
+        with open(api_structures_path,'w') as jsonFile:
+            json.dump(structures_data, jsonFile)
+    else:
+        print("An error occurred while trying to retrieve structure data")
--- geopandas-playground.py
+++ geopandas-playground.py
@@ -1,8 +1,65 @@
-import geopandas
+import geopandas as gpd
+from datasets import api_hydrants_path, api_zones_path, api_structures_path, request_API_data
 
-# Gets hydrant coordinates from a .json and stores them in a .geoJson file
-data_path = "/Users/john/Documents/College/Courses/CS/CS701/FireDeserts/hydrants.json"
-gdf = geopandas.read_file(data_path)
-hydrant_coords = gdf["geometry"]
-print(hydrant_coords)
-hydrant_coords.to_file("hydrant_coords.geojson", driver="GeoJSON")
+# Creates a 1-column geojson file containing fire hydrant coordinates
+# from a json file collected from the Vermont Geoportal API
+def hydrants_to_geojson(output_file_name, file_path):
+    hydrant_path = file_path
+    gdf = gpd.read_file(hydrant_path)
+    # print(gdf.columns)
+
+    # hydrant_data = gdf[["COUNTY", "HYDRANTID", "HYDRANTTYPE", "FLOWRATE", "geometry"]]
+    hydrant_coords = gdf["geometry"]
+
+    # print(hydrant_data)
+    print(hydrant_coords)
+
+    print("Outputting %s.geojson..." % output_file_name)
+    hydrant_coords.to_file("%s.geojson" % output_file_name, driver="GeoJSON")
+
+# Creates a 1-column geojson file containing only the emergency zone polygon coordinates
+# from a json file collected from the Vermont Geoportal API
+def zones_to_geojson(output_file_name, file_path):
+    zone_path = file_path
+    gdf = gpd.read_file(zone_path)
+    # print(gdf.columns)
+
+    # zone_data = gdf[["COUNTY", "ESZID", "FIRE_AgencyId", "ESN", "geometry"]]
+    zone_polygons = gdf["geometry"]
+
+    # print(zone_data)
+    print(zone_polygons)
+
+    print("Outputting %s.geojson..." % output_file_name)
+    zone_polygons.to_file("%s.geojson" % output_file_name, driver="GeoJSON")
+
+
+# Creates a 1-column geojson file containing fire station coordinates and related information
+# from a json file collected from the Vermont Geoportal API
+def stations_to_geojson(output_file_name, file_path):
+    structure_data = file_path
+    gdf = gpd.read_file(structure_data)
+    
+    # station_data = gdf[["COUNTY", "ESN", "ZIP", "ESZ", "geometry"]]
+    station_coords = gdf["geometry"]
+
+    # print(station_data)
+    # print(station_coords)
+    
+    print("Outputting %s.geojson..." % output_file_name)
+    station_coords.to_file("%s.geojson" % output_file_name, driver="GeoJSON")
+    
+
+if __name__ == "__main__":
+
+    # Creates JSON files collected from the API to be ingested by GeoPandas
+    request_API_data()
+
+    # Filter through JSON data for geometry coordinates to be parsed in networkx
+    # and store these as new geojson files whose name is the first argument to
+    # the functions below (e.g., hydrant_coords.geojson).
+    hydrants_to_geojson("hydrant_coords", api_hydrants_path)
+    zones_to_geojson("zone_polygons", api_zones_path)
+    stations_to_geojson("fire_station_coords", api_structures_path)
+
+    
