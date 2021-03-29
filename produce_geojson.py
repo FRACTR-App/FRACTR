9832194b3941f09e1b9571e461e2a651d1e5c188
Halcyon Brown <halcyonb@middlebury.edu>
2021-03-28 22:26:04 -0400

    merging station and service zones into new geojson

--- produce_geojson.py
+++ produce_geojson.py
@@ -27,11 +27,13 @@ def zones_to_geojson(output_file_name, file_path):
     # zone_data = gdf[["COUNTY", "ESZID", "FIRE_AgencyId", "ESN", "geometry"]]
     # print(zone_data)
 
-    zone_polygons = gdf["geometry"]
+    zone_polygons = gdf[["ESN", "geometry"]]
     print(zone_polygons)
 
     print("Outputting %s.geojson..." % output_file_name)
     zone_polygons.to_file("%s.geojson" % output_file_name, driver="GeoJSON")
+
+    return zone_polygons
 
 
 # Creates a 1-column geojson file containing fire station coordinates and related information
@@ -40,24 +42,45 @@ def stations_to_geojson(output_file_name, file_path):
     structure_data = file_path
     gdf = gpd.read_file(structure_data)
     
-    station_coords = gdf.loc[gdf["SITETYPE"].str.contains("FIRE STATION"), "geometry"]
+    station_coords = gdf.loc[gdf["SITETYPE"].str.contains("FIRE STATION"), ["ESN", "geometry"]]
     print(station_coords)
     # print(gdf.SITETYPE.unique())
     
     print("Outputting %s.geojson..." % output_file_name)
     station_coords.to_file("%s.geojson" % output_file_name, driver="GeoJSON")
+    return station_coords
+
+# Creates a geojson file that includes the merged service zone and fire station index
+# This does not merge both of the geometries, instead it keeps the zone geometry and the station index
+def merged_to_geojson(output_file_name, file_path_1, file_path_2):
+    zone_data = file_path_1
+    station_data = file_path_2
+    gdf_zone = gpd.read_file(zone_data)
+    gdf_station = gpd.read_file(station_data)
+
+    #merge the dataframes
+    merged = gpd.sjoin(gdf_zone, gdf_station, how="inner", op="intersects")
+    print(merged)
     
+    print("Outputting %s.geojson..." % output_file_name)
+    merged.to_file("%s.geojson" % output_file_name, driver="GeoJSON")
+    return merged
+    
+
 
 if __name__ == "__main__":
 
     # Creates JSON files collected from the API to be ingested by GeoPandas
-    request_API_data()
+    # request_API_data()
 
     # Filter through JSON data for geometry coordinates to be parsed in networkx
     # and store these as new geojson files whose name is the first argument to
     # the functions below (e.g., hydrant_coords.geojson).
-    hydrants_to_geojson("hydrant_coords", api_hydrants_path)
+    # hydrants_to_geojson("hydrant_coords", api_hydrants_path)
+    
     zones_to_geojson("zone_polygons", api_zones_path)
     stations_to_geojson("fire_station_coords", api_structures_path)
+    merged = merged_to_geojson("merged_zones_stations", 'zone_polygons.geojson', 'fire_station_coords.geojson')
+    print(merged)
 
     
