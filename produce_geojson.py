c37fc4d16fe233a4815d0b31a8d2b408962f7993
John Cambefort <jcambefort@middlebury.edu>
2021-04-02 21:55:15 -0400

    Comments & strucutre

--- produce_geojson.py
+++ produce_geojson.py
@@ -1,10 +1,12 @@
 import geopandas as gpd
+import osmnx as ox
 from datasets import api_hydrants_path, api_zones_path, api_structures_path, request_API_data
 
 # Creates a 1-column geojson file containing fire hydrant coordinates
 # from a json file collected from the Vermont Geoportal API
-def hydrants_to_geojson(output_file_name, file_path):
-    hydrant_path = file_path
+# Returns the GeoDataFrame (used to generate the output .geojson)
+def hydrants_to_geojson(output_file_name, input_file_path):
+    hydrant_path = input_file_path
     gdf = gpd.read_file(hydrant_path)
     # print(gdf.columns)
 
@@ -19,8 +21,9 @@ def hydrants_to_geojson(output_file_name, file_path):
 
 # Creates a 1-column geojson file containing only the emergency zone polygon coordinates
 # from a json file collected from the Vermont Geoportal API
-def zones_to_geojson(output_file_name, file_path):
-    zone_path = file_path
+# Returns the GeoDataFrame (used to generate the output .geojson)
+def zones_to_geojson(output_file_name, input_file_path):
+    zone_path = input_file_path
     gdf = gpd.read_file(zone_path)
     # print(gdf.columns)
 
@@ -38,8 +41,9 @@ def zones_to_geojson(output_file_name, file_path):
 
 # Creates a 1-column geojson file containing fire station coordinates and related information
 # from a json file collected from the Vermont Geoportal API
-def stations_to_geojson(output_file_name, file_path):
-    structure_data = file_path
+# Returns the GeoDataFrame (used to generate the output .geojson)
+def stations_to_geojson(output_file_name, input_file_path):
+    structure_data = input_file_path
     gdf = gpd.read_file(structure_data)
     
     station_coords = gdf.loc[gdf["SITETYPE"].str.contains("FIRE STATION"), ["ESN", "geometry"]]
@@ -71,16 +75,15 @@ def merged_to_geojson(output_file_name, file_path_1, file_path_2):
 if __name__ == "__main__":
 
     # Creates JSON files collected from the API to be ingested by GeoPandas
-    # request_API_data()
+    request_API_data()
 
-    # Filter through JSON data for geometry coordinates to be parsed in networkx
-    # and store these as new geojson files whose name is the first argument to
-    # the functions below (e.g., hydrant_coords.geojson).
-    # hydrants_to_geojson("hydrant_coords", api_hydrants_path)
+    # Create new GEOJSON files by filtering through JSON data for relevant columns,
+    # output geojson files
     
+    hydrants_to_geojson("hydrant_coords", api_hydrants_path)
     zones_to_geojson("zone_polygons", api_zones_path)
     stations_to_geojson("fire_station_coords", api_structures_path)
-    merged = merged_to_geojson("merged_zones_stations", 'zone_polygons.geojson', 'fire_station_coords.geojson')
-    print(merged)
+    # merged = merged_to_geojson("merged_zones_stations", 'zone_polygons.geojson', 'fire_station_coords.geojson')
+    # print(merged)
 
     
