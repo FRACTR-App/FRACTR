9aa78419fe1f5a2ee443e846628f9ecfe45a2b0b
John Cambefort <jcambefort@middlebury.edu>
2021-04-04 21:34:19 -0400

    Initial isochrone map with Middlebury North Station

--- network_analysis.py
+++ network_analysis.py
@@ -12,7 +12,7 @@ import geopandas as gpd
 from shapely.geometry import Polygon, Point, LineString
 import matplotlib.pyplot as plt
 from descartes import PolygonPatch
-from IPython.display import IFrame
+# from IPython.display import IFrame
 
 ox.config(log_console=False, use_cache=True)
 
@@ -29,16 +29,17 @@ print(station_one_geom)
 station_tuple = (station_one_geom.y, station_one_geom.x)
 print(station_tuple)
 
-def create_graph(loc, dist, transport_mode, loc_type="points"):
-    # Transport mode = ‘walk’, ‘bike’, ‘drive’, ‘drive_service’, ‘all’, ‘all_private’, ‘none’
-    if loc_type == "address":
-        G = ox.graph_from_address(loc, distance=dist, dist_type="network", network_type=transport_mode)
-    elif loc_type == "points":
-        G = ox.graph_from_point(loc, distance=dist, dist_type="network", network_type=transport_mode)
-    return G
+# def create_graph(loc, distance, transport_mode, loc_type="points"):
+#     # Transport mode = ‘walk’, ‘bike’, ‘drive’, ‘drive_service’, ‘all’, ‘all_private’, ‘none’
+#     if loc_type == "address":
+#         G = ox.graph_from_address(loc, dist=distance, dist_type="network", network_type=transport_mode)
+#     elif loc_type == "points":
+#         G = ox.graph_from_point(loc, dist=distance, dist_type="network", network_type=transport_mode)
+#     return G
 
 # 1 - Create a graph
-G = ox.graph_from_point(station_tuple, dist=20000, dist_type='network', network_type='drive_service')
+G = ox.graph_from_polygon(zone_geom, network_type='drive_service')
+# G = ox.graph_from_point(station_tuple, dist=10000, dist_type='network', network_type='all')
 # 2 - Create nodes geodataframe from Graph network (G)
 gdf_nodes = ox.graph_to_gdfs(G, edges=False)
 # 3 - Specify where you want to start and get nearest nodes. 
@@ -46,50 +47,77 @@ station_of_interest = ox.get_nearest_node(G, point=station_tuple)
 
 # Pass in a few default speed values (km/hour)
 # to fill in edges with missing `maxspeed` from OSM
-hwy_speeds = {"residential": 40, "secondary": 56, "tertiary": 80}
+hwy_speeds = {"residential": 40, 
+              "unclassified": 40,
+              "tertiary": 56, 
+              "secondary": 56, 
+              "primary": 80, 
+              "trunk": 56
+            }
+# # 25 mph, 35 mph, 50 mph
    
 G = ox.add_edge_speeds(G, hwy_speeds)
-G = ox.add_edge_travel_times(G)
 
-
+# For every edge, add a 'time' attribute in minutes.
+# This is based on 'length' (originally in meters, converted here to km) 
+# and 'speed_kph' (added to the edge dict by add_edge_speeds(),
+# and converted here to km/m).
+for _, _, _, data in G.edges(data=True, keys=True):
+    data["time"] = (data["length"] / 1000) / (data["speed_kph"] / 60) #km/m
+    print(data)
 
 # 4 - Project a graph from lat-long to the UTM zone appropriate for its geographic location.
 G = ox.project_graph(G)
 
-# Response time in mintues
-response_times = [0.5, 1, 1.5, 2, 3, 5]
-for i in range(len(response_times)):
-   response_times[i] = response_times[i] * 60
-print(response_times)
+# Response time in minutes
+response_times = [2, 5]
 
 # 1 - get one color for each isochrone
 iso_colors = ox.plot.get_colors(n=len(response_times), cmap='Reds', start=0.3, return_hex=True)
 # 2 - color the nodes according to isochrone then plot the street network
 node_colors = {}
+
+# Iterate over response times and possible colors
+# Create subgraphs with nodes within radius (using time attributes of edges rather than a spatial distance)
+# Associate nodes in the same subgraph to the same color
 for trip_time, color in zip(sorted(response_times, reverse=True), iso_colors):
     subgraph = nx.ego_graph(G, station_of_interest, radius=trip_time, distance='time')
     for node in subgraph.nodes():
         node_colors[node] = color
+
+# Get list of possible node colors
 nc = [node_colors[node] if node in node_colors else 'none' for node in G.nodes()]
+# Get list of possible node sizes (i.e. whether or not we should display them)
 ns = [20 if node in node_colors else 0 for node in G.nodes()]
+
 fig, ax = ox.plot_graph(G, node_color=nc, node_size=ns, node_alpha=0.8, edge_linewidth=0.2,
-    edge_color="#999999",)
+    edge_color="#999999")
 
 
-# # make the isochrone polygons
-# isochrone_polys = []
-# for trip_time in sorted(response_times, reverse=True):
-#     subgraph = nx.ego_graph(G, station_of_interest, radius=trip_time, distance='time')
-#     node_points = [Point((data['x'], data['y'])) for node, data in subgraph.nodes(data=True)]
-#     bounding_poly = gpd.GeoSeries(node_points).unary_union.convex_hull
-#     isochrone_polys.append(bounding_poly)
+# make the isochrone polygons
+isochrone_polys = []
+for response_time in sorted(response_times, reverse=True):
+    subgraph = nx.ego_graph(G, station_of_interest, radius=response_time, distance='time')
+    node_points = [Point((data['x'], data['y'])) for node, data in subgraph.nodes(data=True)]
+    bounding_poly = gpd.GeoSeries(node_points).unary_union.convex_hull
+    isochrone_polys.append(bounding_poly)
 
-# # plot the network then add isochrones as colored descartes polygon patches
-# fig, ax = ox.plot_graph(G, show=False, close=False, edge_color='k', edge_alpha=0.2, node_color='none')
-# for polygon, fc in zip(isochrone_polys, iso_colors):
-#     patch = PolygonPatch(polygon, fc=fc, ec='none', alpha=0.6, zorder=-1)
-#     ax.add_patch(patch)
-# plt.show()
+# plot the network then add isochrones as colored descartes polygon patches
+fig, ax = ox.plot_graph(G, show=False, close=False, edge_color='k', edge_alpha=0.2, node_color='none')
+for polygon, fc in zip(isochrone_polys, iso_colors):
+    patch = PolygonPatch(polygon, fc=fc, ec='none', alpha=0.6, zorder=-1)
+    ax.add_patch(patch)
+plt.show()
 
+"""
+Questions for Joe:
+- Nodes are currently road intersections, not buildings
+- Convex vs concave hulls
+- How to handle overlap?
+- Best way to 'export' the polygons together (into a layer?)?
+  Export as one vector layer of multiple polygons? Raster heat map? (rasters are pixel-based,
+  vectors are point-based / have nodes, edges and polygons)
+- ESN s
 
+"""
 
