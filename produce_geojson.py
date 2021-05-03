2e593fa6d9fd85973fc57d4276cb08cbf0a0d6ef
Halcyon Brown <halcyonb@middlebury.edu>
2021-05-03 14:25:26 -0400

    produce new hydrant geojson

--- produce_geojson.py
+++ produce_geojson.py
@@ -7,10 +7,10 @@ from datasets import API_HYDRANTS_PATH, API_ZONES_PATH, API_STRUCTURES_PATH, API
 def hydrants_to_geojson(output_file_name, input_file_path):
     hydrant_path = input_file_path
     gdf = gpd.read_file(hydrant_path)
-    # hydrant_data = gdf[["COUNTY", "HYDRANTID", "HYDRANTTYPE", "FLOWRATE", "geometry"]]
+    hydrant_coords = gdf[["COUNTY", "HYDRANTID", "HYDRANTTYPE", "FLOWRATE", "geometry"]]
 
-    hydrant_coords = gdf["geometry"]
-    print(hydrant_coords)
+    #hydrant_coords = gdf["geometry"]
+    #print(hydrant_coords)
 
     print("Outputting %s.geojson..." % output_file_name)
     hydrant_coords.to_file("%s.geojson" % output_file_name, driver="GeoJSON")
@@ -76,8 +76,8 @@ if __name__ == "__main__":
 
     # Create new GEOJSON files by filtering through JSON data for relevant columns,
     # output geojson files
-    stations_to_geojson("fire_station_coords", API_STRUCTURES_PATH)
+    #stations_to_geojson("fire_station_coords", API_STRUCTURES_PATH)
     # vermont_to_geojson("vermont_state_polygon", API_VERMONT_PATH)
     # zone_to_geojson("zone_polygons", API_ZONES_PATH)
-    
+    hydrants_to_geojson("hydrant_coords.geojson", API_HYDRANTS_PATH)
     
