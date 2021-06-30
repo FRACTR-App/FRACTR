a527f4e86d1b09ef0ca7f053ac197ba14591f68a
Halcyon Brown <halcyonb@middlebury.edu>
2021-06-30 08:18:20 -0400

    department types backend done

--- dept_type_to_json.py
+++ dept_type_to_json.py
@@ -27,39 +27,3 @@ def csv_to_json(csvFilePath, jsonFilePath):
 csvFilePath = r'department_types.csv'
 jsonFilePath = r'department_types.json'
 csv_to_json(csvFilePath, jsonFilePath)
-
-# import csv
-# import json
-
-
-# # Function to convert a CSV to JSON
-# # Takes the file paths as arguments
-# def make_json(csvFilePath, jsonFilePath):
-	
-# 	# create a dictionary
-# 	data = {}
-	
-# 	# Open a csv reader called DictReader
-# 	with open(csvFilePath, encoding='utf-8') as csvf:
-# 		csvReader = csv.DictReader(csvf)
-		
-# 		# Convert each row into a dictionary
-# 		# and add it to data
-# 		for rows in csvReader:
-			
-# 			data.append(rows)
-
-# 	# Open a json writer, and use the json.dumps()
-# 	# function to dump data
-# 	with open(jsonFilePath, 'w', encoding='utf-8') as jsonf:
-# 		jsonf.write(json.dumps(data, indent=4))
-		
-# # Driver Code
-
-# # Decide the two file paths according to your
-# # computer system
-# csvFilePath = r'department_types.csv'
-# jsonFilePath = r'department_types.json'
-
-# # Call the make_json function
-# make_json(csvFilePath, jsonFilePath)
